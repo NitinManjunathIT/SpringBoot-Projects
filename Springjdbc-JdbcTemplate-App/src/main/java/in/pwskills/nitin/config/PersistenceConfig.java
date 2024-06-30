7f7a93c09e60074b64755d3fe838d6aec90276a3
Nitin <javabynitin2022@gmail.com>
2024-06-30 11:56:26 +0530

    NamedParameterJdbcTemplate

--- Springjdbc-JdbcTemplate-App/src/main/java/in/pwskills/nitin/config/PersistenceConfig.java
+++ Springjdbc-JdbcTemplate-App/src/main/java/in/pwskills/nitin/config/PersistenceConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -25,4 +26,8 @@ public class PersistenceConfig {
 		return new JdbcTemplate(dataSource);
 	}
 
+	@Bean
+	public NamedParameterJdbcTemplate nmJdbcTemplate(@Autowired DataSource dataSource) {
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
 }
